17604d44959629f68b7781894e226c7d37728b35
Suraj N. Kurapati <sunaku@gmail.com>
2010-07-18 12:24:37 -0700

    rename dfect/full to dfect/long and refactor code

---
+++ lib/dfect/long.rb
@@ -0,0 +1,26 @@
+# Provides long name aliases for Dfect's default abbreviated vocabulary.
+
+require 'dfect'
+
+module Dfect
+  short_to_long = {
+    'D' => 'Describe',
+    'T' => 'True',
+    'F' => 'False',
+    'E' => 'Error',
+    'C' => 'Catch',
+    'S' => 'Share',
+    'L' => 'Log',
+  }
+
+  short_names = instance_methods(false).grep(/^[#{short_to_long.keys.join}]\b/)
+
+  short_to_long.each do |prefix, long|
+    short_names.grep(/^#{prefix}/).each do |short|
+      alias_method short.to_s.sub(prefix, long), short
+    end
+  end
+
+  # for hooks
+  Describe = D
+end
